15d17f034e4e1211e40611fc444f0fa1bccdcbc8
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2025-06-18 08:45:26 -0300

    feat: semana 15, acesso a bando de dados - Criação do database.

---
+++ src/main/java/br/edu/ifsul/cstsi/lpoo_objetivo6_aulas/LpooObjetivo6AulasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.lpoo_objetivo6_aulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpooObjetivo6AulasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LpooObjetivo6AulasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsul/cstsi/lpoo_objetivo6_aulas/LpooObjetivo6AulasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.lpoo_objetivo6_aulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpooObjetivo6AulasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
